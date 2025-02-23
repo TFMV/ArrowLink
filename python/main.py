443f1bfe92c58a280b579e8e4770fdc7fa7b7215
TFMV <mcgeehan@gmail.com>
2025-02-23 11:57:24 -0600

    Harden gRPC Server

--- python/main.py
+++ python/main.py
@@ -1,9 +1,8 @@
 import grpc
 import pyarrow as pa
 import pyarrow.ipc as ipc
-import pandas as pd
 from proto.dataexchange_pb2_grpc import ArrowDataServiceStub
-from proto.dataexchange_pb2 import ArrowData, Empty
+from proto.dataexchange_pb2 import Empty
 
 def run():
     channel = grpc.insecure_channel('localhost:50051')
@@ -12,10 +11,6 @@ def run():
     response_stream = stub.GetArrowData(Empty())
     
     for response in response_stream:
-        # Write the response to a file
-        with open('response.arrow', 'wb') as f:
-            f.write(response.payload)
-
         reader = ipc.RecordBatchStreamReader(pa.BufferReader(response.payload))
         table = reader.read_all()
         print(table)
