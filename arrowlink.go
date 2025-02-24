c7dd24b7fd03a029f3c902664906ad67df112516
TFMV <mcgeehan@gmail.com>
2025-02-23 20:04:45 -0600

    Simplify

--- arrowlink.go
+++ arrowlink.go
@@ -1,30 +1,10 @@
 package main
 
 import (
-	"log"
-	"net"
-
 	"github.com/TFMV/ArrowLink/arrow"
-	pb "github.com/TFMV/ArrowLink/proto/dataexchange"
+	"github.com/TFMV/ArrowLink/grpcserver"
 	"go.uber.org/zap"
-	"google.golang.org/grpc"
 )
-
-type server struct {
-	pb.UnimplementedArrowDataServiceServer
-	logger       *zap.Logger
-	arrowService arrow.ArrowService
-}
-
-func (s *server) GetArrowData(req *pb.Empty, stream pb.ArrowDataService_GetArrowDataServer) error {
-	data, err := s.arrowService.GetData()
-	if err != nil {
-		s.logger.Error("failed to get arrow data", zap.Error(err))
-		return err
-	}
-
-	return stream.Send(&pb.ArrowData{Payload: data})
-}
 
 func main() {
 	// Create zap logger (production configuration)
@@ -37,21 +17,6 @@ func main() {
 	// Create arrow service
 	arrowService := arrow.NewArrowService()
 
-	// Create listener
-	lis, err := net.Listen("tcp", ":50051")
-	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
-	}
-
-	// Create gRPC server
-	s := grpc.NewServer()
-	pb.RegisterArrowDataServiceServer(s, &server{
-		logger:       logger,
-		arrowService: arrowService,
-	})
-
-	logger.Info("Starting gRPC server on :50051")
-	if err := s.Serve(lis); err != nil {
-		logger.Fatal("failed to serve", zap.Error(err))
-	}
+	// Start the gRPC server with our arrowService injected.
+	grpcserver.StartGRPCServer(":50051", logger, arrowService)
 }
--- grpcserver/grpcserver.go
+++ grpcserver/grpcserver.go
@@ -6,26 +6,43 @@ import (
 	"os/signal"
 	"syscall"
 
+	"github.com/TFMV/ArrowLink/arrow"
+	pb "github.com/TFMV/ArrowLink/proto/dataexchange"
 	"go.uber.org/zap"
 
 	grpc_middleware "github.com/grpc-ecosystem/go-grpc-middleware"
 	grpc_zap "github.com/grpc-ecosystem/go-grpc-middleware/logging/zap"
 	grpc_recovery "github.com/grpc-ecosystem/go-grpc-middleware/recovery"
-
-	pb "github.com/TFMV/ArrowLink/proto/dataexchange"
 	"google.golang.org/grpc"
 )
 
+// Server implements the ArrowDataServiceServer interface and holds an arrow service instance.
 type Server struct {
 	pb.ArrowDataServiceServer
-	logger *zap.Logger
+	logger       *zap.Logger
+	arrowService arrow.ArrowService
 }
 
-func NewServer(logger *zap.Logger) *Server {
-	return &Server{logger: logger}
+// NewServer creates a new Server instance.
+func NewServer(logger *zap.Logger, arrowService arrow.ArrowService) *Server {
+	return &Server{
+		logger:       logger,
+		arrowService: arrowService,
+	}
 }
 
-func StartGRPCServer(address string, logger *zap.Logger) {
+// GetArrowData retrieves the Arrow data and streams it to the client.
+func (s *Server) GetArrowData(req *pb.Empty, stream pb.ArrowDataService_GetArrowDataServer) error {
+	data, err := s.arrowService.GetData()
+	if err != nil {
+		s.logger.Error("failed to get arrow data", zap.Error(err))
+		return err
+	}
+	return stream.Send(&pb.ArrowData{Payload: data})
+}
+
+// StartGRPCServer sets up and runs the gRPC server with middleware and graceful shutdown.
+func StartGRPCServer(address string, logger *zap.Logger, arrowService arrow.ArrowService) {
 	grpc_zap.ReplaceGrpcLogger(logger)
 
 	opts := []grpc.ServerOption{
@@ -40,13 +57,14 @@ func StartGRPCServer(address string, logger *zap.Logger) {
 	}
 
 	grpcServer := grpc.NewServer(opts...)
-	pb.RegisterArrowDataServiceServer(grpcServer, NewServer(logger))
+	pb.RegisterArrowDataServiceServer(grpcServer, NewServer(logger, arrowService))
 
 	lis, err := net.Listen("tcp", address)
 	if err != nil {
 		logger.Fatal("failed to listen", zap.Error(err))
 	}
 
+	// Run server in a separate goroutine.
 	go func() {
 		logger.Info("gRPC server is running", zap.String("address", address))
 		if err := grpcServer.Serve(lis); err != nil {
@@ -54,6 +72,7 @@ func StartGRPCServer(address string, logger *zap.Logger) {
 		}
 	}()
 
+	// Listen for termination signals to gracefully shut down.
 	quit := make(chan os.Signal, 1)
 	signal.Notify(quit, os.Interrupt, syscall.SIGTERM)
 	<-quit
